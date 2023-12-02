0298ec7f60ed927ad83acf9cf7d0414990e61359
jamsidedown <46314954+jamsidedown@users.noreply.github.com>
2023-12-02 12:03:05 +0000

    Solution for day 2

--- lib/days/day02.ml
+++ lib/days/day02.ml
@@ -1,6 +1,86 @@
+open Operators
+
+type round = {
+    red: int;
+    green: int;
+    blue: int
+}
+
+type game = {
+    game_id: int;
+    rounds: round list
+}
+
+let parse_round (s: string) : round =
+    let rec recurse (current: round) (parts: string list) : round =
+        match parts with
+        | [] -> current
+        | head :: tail ->
+            (match head |> String.trim |> String.split_on_char ' ' with
+            | [ count; "red" ] -> recurse {current with red=(int_of_string count)} tail
+            | [ count; "green" ] -> recurse {current with green=(int_of_string count)} tail
+            | [ count; "blue" ] -> recurse {current with blue=(int_of_string count)} tail
+            | _ -> recurse current tail)
+    in
+    String.split_on_char ',' s
+    |> List.map String.trim
+    |> recurse {red=0; green=0; blue=0};;
+
+let parse_game (line: string) : game option =
+    match String.split_on_char ':' line with
+    | [ game_info; rounds_info ] ->
+        let game_id = Scanf.sscanf game_info "Game %i" (fun x -> x) in
+        (match String.split_on_char ';' rounds_info with
+        | [] -> None
+        | rounds_str ->
+            let rounds = rounds_str |> List.map parse_round in
+            Some { game_id=game_id; rounds=rounds })
+    | _ -> None;;
+
+let is_possible_game (game: game) (max_round: round) : bool =
+    game.rounds
+    |> List.for_all (fun round ->
+        round.red <= max_round.red
+        && round.green <= max_round.green
+        && round.blue <= max_round.blue);;
+
 let part_one (lines: string list) : int =
-    List.length lines;;
+    let game_limit = { red=12; green=13; blue=14 } in
+    lines
+    |> List.map parse_game
+    |> List.choose
+    |> List.filter (fun game -> is_possible_game game game_limit)
+    |> List.map (fun game -> game.game_id)
+    |> List.sum;;
+
+let max_round (first: round) (second: round) : round =
+    {
+        red=max first.red second.red;
+        green=max first.green second.green;
+        blue=max first.blue second.blue;
+    }
+
+let get_minumum_cubes (game: game) : round =
+    let rec recurse (current: round) (rounds: round list) : round =
+        match rounds with
+        | [] -> current
+        | head :: tail ->
+            let next = max_round current head in
+            recurse next tail
+    in
+    recurse { red=0; green=0; blue=0; } game.rounds;;
+
+let get_power (round: round) : int =
+    round.red * round.green * round.blue;;
+
+let part_two (lines: string list) : int =
+    lines
+    |> List.map parse_game
+    |> List.choose
+    |> List.map (get_minumum_cubes >> get_power)
+    |> List.sum;;
 
 let run () =
     let input = File.read_lines "day02.txt" in
-    input |> List.iter (print_endline);;
+    input |> part_one |> Printf.printf "Part one: %i\n";
+    input |> part_two |> Printf.printf "Part two: %i\n";;
--- test/day02_tests.ml
+++ test/day02_tests.ml
@@ -1,11 +1,102 @@
 open OUnit2
 open Aoc23
 
-let can_solve_sample_for_part_one _ =
-    Day02.part_one ["hello"] |> ignore;
-    assert true;;
+let can_parse_first_game_id _ =
+    let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green" in
+    let game = Day02.parse_game input in
+    match game with
+    | Some g -> assert_equal g.game_id 1
+    | _ -> assert false;;
+
+let can_parse_second_game_id _ =
+    let input = "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue" in
+    let game = Day02.parse_game input in
+    match game with
+    | Some g -> assert_equal g.game_id 2
+    | _ -> assert false;;
+
+let can_parse_first_round _ =
+    let input = " 3 blue, 4 red" in
+    let round = Day02.parse_round input in
+    assert_equal round { red= 4; green=0; blue=3 };;
+
+let can_parse_first_game _ =
+    let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green" in
+    let game = Day02.parse_game input in
+    assert_equal game (Some { game_id=1; rounds= [
+        { red=4; green=0; blue=3 };
+        { red=1; green=2; blue=6 };
+        { red=0; green=2; blue=0 }
+    ] });;
+
+let can_check_that_game_is_possible _ =
+    let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green" in
+    let game = Day02.parse_game input in
+    match game with
+    | None -> assert false
+    | Some g ->
+        let max_round: Day02.round = { red=12; green=13; blue=14 } in
+        let possible = Day02.is_possible_game g max_round in
+        assert_equal possible true;;
+
+let can_check_that_game_is_impossible _ =
+    let input = "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red" in
+    let game = Day02.parse_game input in
+    match game with
+    | None -> assert false
+    | Some g ->
+        let max_round: Day02.round = { red=12; green=13; blue=14 } in
+        let possible = Day02.is_possible_game g max_round in
+        assert_equal possible false;;
+
+let can_solve_part_one _ =
+    let input = [
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue";
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red";
+        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red";
+        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+    ] in
+    let result = Day02.part_one input in
+    assert_equal result 8;;
+
+let can_get_minimum_cubes_for_first_game _ =
+    match Day02.parse_game "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green" with
+    | None -> assert false
+    | Some game ->
+        let expected: Day02.round = { red=4; green=2; blue=6 } in
+        let minimum = Day02.get_minumum_cubes game in
+        assert_equal minimum expected;;
+
+let can_calculate_power_of_first_game _ =
+    match Day02.parse_game "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green" with
+    | None -> assert false
+    | Some game ->
+        let minimum = Day02.get_minumum_cubes game in
+        let power = Day02.get_power minimum in
+        assert_equal power 48;;
+
+let can_solve_part_two _ =
+    let input = [
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue";
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red";
+        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red";
+        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+    ] in
+    let result = Day02.part_two input in
+    assert_equal result 2286;;
 
 let tests =
     "day 2" >::: [
-        "can solve sample input for part one" >:: can_solve_sample_for_part_one;
+        "can parse game id from first sample line" >:: can_parse_first_game_id;
+        "can parse game id from second sample line" >:: can_parse_second_game_id;
+        "can parse first round" >:: can_parse_first_round;
+        "can parse first game" >:: can_parse_first_game;
+        "can check that game is possible" >:: can_check_that_game_is_possible;
+        "can check that game is impossible" >:: can_check_that_game_is_impossible;
+        "can solve part one" >:: can_solve_part_one;
+        "can get minumum cubes for first game" >:: can_get_minimum_cubes_for_first_game;
+        "can calculate power of first game" >:: can_calculate_power_of_first_game;
+        "can solve part two" >:: can_solve_part_two;
     ]
