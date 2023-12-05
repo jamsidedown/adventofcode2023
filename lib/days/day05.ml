62ff498d53fae6b17a00cc7b88e77f861dbba740
jamsidedown <46314954+jamsidedown@users.noreply.github.com>
2023-12-05 13:11:51 +0000

    Solution to day 5 part 1

--- lib/days/day05.ml
+++ lib/days/day05.ml
@@ -11,24 +11,77 @@ type map = {
     ranges: range list;
 }
 
-let parse_seeds (line: string) : int list =
+let parse_ints (line: string) : int list =
     match String.split_on_char ' ' line with
     | [] -> []
-    | _ :: tail ->
-        tail |> List.map (String.trim >> int_of_string);;
+    | lst -> lst |> List.map (String.trim >> int_of_string);;
 
-let parse_maps (_: string list) : map list =
-    [];;
+let parse_seeds (line: string) : int list =
+    match String.split_on_char ':' line with
+    | [_; tail] -> tail |> String.trim |> parse_ints
+    | _ -> [];;
 
-let apply_range (range: range) (value: int) : int =
+let parse_name (line: string) : string =
+    Scanf.sscanf line "%s map:" (fun x -> x);;
+
+let parse_range (line: string) : range option =
+    match parse_ints line with
+    | [dest; src; length] -> Some {dest=dest; src=src; length=length}
+    | _ -> None
+
+let parse_maps (lines: string list) : map list =
+    let rec recurse (name: string) (ranges: range list) (lines: string list) : map list =
+        match lines with
+        | "" :: tail ->
+            (match ranges with
+            | [] -> recurse "" [] tail
+            | _ -> {name=name; ranges=List.rev ranges} :: recurse "" [] tail)
+        | head :: tail when name = "" -> recurse (parse_name head) [] tail
+        | head :: tail ->
+            let range = parse_range head in
+            (match range with
+            | Some range -> recurse name (range :: ranges) tail
+            | None -> recurse name ranges tail)
+        | [] when name = "" -> []
+        | _ -> {name=name; ranges=List.rev ranges} :: []
+    in
+    recurse "" [] lines;;
+
+let apply_range (range: range) (value: int) : int option =
     let diff = value - range.src in
-    if diff >= 0 && diff < range.length then range.dest + diff else value;;
+    if diff >= 0 && diff < range.length then Some (range.dest + diff) else None;;
 
-let part_one (_: string list) : int =
-    0;;
+let apply_map (map: map) (value: int) : int =
+    let rec recurse (ranges: range list) : int =
+        match ranges with
+        | [] -> value
+        | head :: tail ->
+            match apply_range head value with
+            | Some x -> x
+            | None -> recurse tail
+    in
+    recurse map.ranges;;
+
+let rec apply_maps (maps: map list) (value: int) : int =
+    match maps with
+    | map :: tail -> apply_map map value |> apply_maps tail
+    | [] -> value;;
+
+let part_one (lines: string list) : int =
+    match lines with
+    | [] -> 0
+    | head :: tail ->
+        let seeds = parse_seeds head in
+        let maps = parse_maps tail in
+        seeds
+        |> List.map (fun seed -> apply_maps maps seed)
+        |> List.sort compare
+        |> List.hd;;
 
 let part_two (_: string list) : int =
     0;;
 
 let run () =
-    print_endline "day 5";;
+    let input = File.read_lines "day05.txt" in
+    input |> part_one |> Printf.printf "Part one: %i\n";
+    input |> part_two |> Printf.printf "Part two: %i\n";;
--- test/day05_tests.ml
+++ test/day05_tests.ml
@@ -1,6 +1,42 @@
 open OUnit2
 open Aoc23
 
+let sample_input = [
+    "seeds: 79 14 55 13";
+    "";
+    "seed-to-soil map:";
+    "50 98 2";
+    "52 50 48";
+    "";
+    "soil-to-fertilizer map:";
+    "0 15 37";
+    "37 52 2";
+    "39 0 15";
+    "";
+    "fertilizer-to-water map:";
+    "49 53 8";
+    "0 11 42";
+    "42 0 7";
+    "57 7 4";
+    "";
+    "water-to-light map:";
+    "88 18 7";
+    "18 25 70";
+    "";
+    "light-to-temperature map:";
+    "45 77 23";
+    "81 45 19";
+    "68 64 13";
+    "";
+    "temperature-to-humidity map:";
+    "0 69 1";
+    "1 0 69";
+    "";
+    "humidity-to-location map:";
+    "60 56 37";
+    "56 93 4"
+]
+
 let test_can_parse_sample_seeds _ =
     let line = "seeds: 79 14 55 13" in
     let seeds = Day05.parse_seeds line in
@@ -8,10 +44,33 @@ let test_can_parse_sample_seeds _ =
 
 let test_can_apply_first_sample_range _ =
     let range: Day05.range = {dest=50; src=98; length=2} in
-    assert_equal (Day05.apply_range range 97) 97;
-    assert_equal (Day05.apply_range range 98) 50;
-    assert_equal (Day05.apply_range range 99) 51;
-    assert_equal (Day05.apply_range range 100) 100;;
+    assert_equal (Day05.apply_range range 97) None;
+    assert_equal (Day05.apply_range range 98) (Some 50);
+    assert_equal (Day05.apply_range range 99) (Some 51);
+    assert_equal (Day05.apply_range range 100) None;;
+
+let test_can_apply_first_sample_map _ =
+    let map: Day05.map = {
+        name="seed-to-soil";
+        ranges=[
+            {dest=50; src=98; length=2};
+            {dest=52; src=50; length=48}
+        ]
+    } in
+    assert_equal (Day05.apply_map map 97) 99;
+    assert_equal (Day05.apply_map map 98) 50;
+    assert_equal (Day05.apply_map map 99) 51;
+    assert_equal (Day05.apply_map map 100) 100;;
+
+let test_can_parse_first_sample_range _ =
+    let line = "50 98 2" in
+    let range = Day05.parse_range line in
+    assert_equal range (Some {dest=50; src=98; length=2});;
+
+let test_can_parse_first_sample_name _ =
+    let line = "seed-to-soil map:" in
+    let name = Day05.parse_name line in
+    assert_equal name "seed-to-soil";;
 
 let test_can_parse_first_sample_map _ =
     let lines = [
@@ -30,9 +89,19 @@ let test_can_parse_first_sample_map _ =
         | _ -> assert false)
     | _ -> assert false;;
 
+let test_can_solve_part_one _ =
+    let result = Day05.part_one sample_input in
+    assert_equal result 35;;
+
+
+
 let tests =
-    "day 4" >::: [
+    "day 5" >::: [
         "can parse seeds from sample" >:: test_can_parse_sample_seeds;
-        "can map seed to soil from sample" >:: test_can_apply_first_sample_range;
-        "can parse first sample maps" >:: test_can_parse_first_sample_map;
+        "can apply first range from sample" >:: test_can_apply_first_sample_range;
+        "can apply first map from sample" >:: test_can_apply_first_sample_map;
+        "can parse first sample range" >:: test_can_parse_first_sample_range;
+        "can parse first sample name" >:: test_can_parse_first_sample_name;
+        "can parse first sample map" >:: test_can_parse_first_sample_map;
+        "can solve part one with the sample data" >:: test_can_solve_part_one;
     ]
