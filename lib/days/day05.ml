1d1885632aadd45b0e3e00b15e2b049ca2b7d9ff
jamsidedown <46314954+jamsidedown@users.noreply.github.com>
2023-12-05 06:35:38 +0000

    Started parsing

--- lib/days/day05.ml
+++ lib/days/day05.ml
@@ -1,3 +1,29 @@
+open Operators
+
+type range = {
+    dest: int;
+    src: int;
+    length: int;
+}
+
+type map = {
+    name: string;
+    ranges: range list;
+}
+
+let parse_seeds (line: string) : int list =
+    match String.split_on_char ' ' line with
+    | [] -> []
+    | _ :: tail ->
+        tail |> List.map (String.trim >> int_of_string);;
+
+let parse_maps (_: string list) : map list =
+    [];;
+
+let apply_range (range: range) (value: int) : int =
+    let diff = value - range.src in
+    if diff >= 0 && diff < range.length then range.dest + diff else value;;
+
 let part_one (_: string list) : int =
     0;;
 
--- test/day05_tests.ml
+++ test/day05_tests.ml
@@ -1,11 +1,38 @@
 open OUnit2
 open Aoc23
 
-let test_can_solve_part_one _ =
-    let result = Day05.part_one [] in
-    assert_equal result 0;;
+let test_can_parse_sample_seeds _ =
+    let line = "seeds: 79 14 55 13" in
+    let seeds = Day05.parse_seeds line in
+    assert_equal seeds [79; 14; 55; 13];;
+
+let test_can_apply_first_sample_range _ =
+    let range: Day05.range = {dest=50; src=98; length=2} in
+    assert_equal (Day05.apply_range range 97) 97;
+    assert_equal (Day05.apply_range range 98) 50;
+    assert_equal (Day05.apply_range range 99) 51;
+    assert_equal (Day05.apply_range range 100) 100;;
+
+let test_can_parse_first_sample_map _ =
+    let lines = [
+        "seed-to-soil map:";
+        "50 98 2";
+        "52 50 48";
+    ] in
+    let maps = Day05.parse_maps lines in
+    match maps with
+    | [m] ->
+        assert_equal m.name "seed-to-soil";
+        (match m.ranges with
+        | [a; b] ->
+            assert_equal a {dest=50; src=98; length=2};
+            assert_equal b {dest=52; src=50; length=48}
+        | _ -> assert false)
+    | _ -> assert false;;
 
 let tests =
     "day 4" >::: [
-        "can solve part one with sample input" >:: test_can_solve_part_one;
+        "can parse seeds from sample" >:: test_can_parse_sample_seeds;
+        "can map seed to soil from sample" >:: test_can_apply_first_sample_range;
+        "can parse first sample maps" >:: test_can_parse_first_sample_map;
     ]
