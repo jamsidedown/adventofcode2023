a5f231f9673668a08ee1ff91b7d1925aa4ce9ac4
jamsidedown <46314954+jamsidedown@users.noreply.github.com>
2023-12-20 12:34:20 +0000

    Solution for day 13

--- lib/days/day13.ml
+++ lib/days/day13.ml
@@ -23,25 +23,70 @@ let mostly_equal (a: int list) (b: int list) : bool =
     | _, [] -> false
     | _ -> recurse a b;;
 
-let is_symmetrical (values: int list) : int option =
+let is_symmetrical ?prev:(prev=(-1)) (values: int list) : int option =
     let rec recurse (seen: int list) (remaining: int list) : int option =
         match remaining with
         | [] -> None
         | head :: tail ->
-            if mostly_equal seen remaining then Some (List.length seen) else recurse (head :: seen) tail
+            let len = List.length seen in
+            if mostly_equal seen remaining && (len <> prev)
+                then Some len
+                else recurse (head :: seen) tail
     in
     recurse [] values;;
 
 let find_symmetry (group: string list) : int =
-    let chars = group |> List.map (String.to_list) in
+    let chars = group |> List.map String.to_list in
     let horizontal = chars |> List.map parse_line in
     let vertical = chars |> List.transpose |> List.map parse_line in
-    (* horizontal |> List.map string_of_int |> String.concat ", " |> Printf.printf "\nhorizontal\n%s\n";
-    vertical |> List.map string_of_int |> String.concat ", " |> Printf.printf "\nvertical\n%s\n"; *)
     match is_symmetrical vertical with
     | Some x -> x
     | None ->
         match is_symmetrical horizontal with
+        | Some x -> x * 100
+        | None -> 0;;
+
+let toggle (row: int) (col: int) (lst: int list) : int list =
+    let rec recurse (seen: int list) (remaining: int list) : int list =
+        if List.length seen = row then
+            (List.rev seen) @ (Int.logxor (List.hd remaining) (Int.shift_left 1 col)) :: (List.tl remaining)
+        else
+            recurse (List.hd remaining :: seen) (List.tl remaining)
+    in
+    recurse [] lst;;
+
+let toggle_bits (prev: int) (width: int) (height: int) (ints: int list) : int option =
+    List.range 0 (width - 1)
+    |> List.map (fun col ->
+        List.range 0 (height - 1)
+        |> List.map (fun row ->
+            toggle row col ints
+            |> is_symmetrical ~prev)
+        |> List.choose)
+    |> List.concat
+    |> function
+        | [] -> None
+        | h :: _ -> Some h;;
+
+let find_alternate_symmetry (group: string list) : int =
+    let chars = group |> List.map String.to_list in
+    let transposed = chars |> List.transpose in
+    let width = chars |> List.hd |> List.length in
+    let height = transposed |> List.hd |> List.length in
+    let horizontal = chars |> List.map parse_line in
+    let vertical = transposed |> List.map parse_line in
+    let prev_vertical =
+        match is_symmetrical vertical with
+        | Some x -> x
+        | None -> -1 in
+    match toggle_bits prev_vertical height width vertical with
+    | Some x -> x
+    | None ->
+        let prev_horizontal =
+            match is_symmetrical horizontal with
+            | Some x -> x
+            | None -> -1 in
+        match toggle_bits prev_horizontal width height horizontal with
         | Some x -> x * 100
         | None -> 0;;
 
@@ -51,8 +96,12 @@ let part_one (lines: string list) : int =
     |> List.map find_symmetry
     |> List.sum;;
 
-let part_two (_: string list) : int =
-    0;;
+let part_two (lines: string list) : int =
+    let groups = split_groups lines in
+    groups
+    |> List.map find_alternate_symmetry
+    |> List.sum;;
+    (* 22067 too low *)
 
 let run () =
     let input = File.read_lines "day13.txt" in
--- test/day13_tests.ml
+++ test/day13_tests.ml
@@ -29,8 +29,18 @@ let test_can_find_symmetry_in_second_sample _ =
     let symmetry = Day13.find_symmetry second_sample_input in
     assert_equal symmetry 400;;
 
+let test_can_find_alternate_symmetry_in_first_sample _ =
+    let symmetry = Day13.find_alternate_symmetry first_sample_input in
+    assert_equal symmetry 300;;
+
+let test_can_find_alternate_symmetry_in_second_sample _ =
+    let symmetry = Day13.find_alternate_symmetry second_sample_input in
+    assert_equal symmetry 100;;
+
 let tests =
     "day 13" >::: [
         "can find vertical symmetry in first sample" >:: test_can_find_symmetry_in_first_sample;
-        "can find vertical symmetry in second sample" >:: test_can_find_symmetry_in_second_sample;
+        "can find horizontal symmetry in second sample" >:: test_can_find_symmetry_in_second_sample;
+        "can find alternate symmetry in first sample" >:: test_can_find_alternate_symmetry_in_first_sample;
+        "can find alternate symmetry in second sample" >:: test_can_find_alternate_symmetry_in_second_sample;
     ]
